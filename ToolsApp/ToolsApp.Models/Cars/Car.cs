031d00fac9ea089079d0cd68e848e053ec724036
Eric Greene <eric@t4d.io>
2021-08-02 18:58:00 -0400

    add exercise 3 solution

---
+++ ToolsApp/ToolsApp.Models/Cars/Car.cs
@@ -0,0 +1,19 @@
+﻿namespace ToolsApp.Models.Cars
+{
+  public record Car(
+    int Id,
+    string Make,
+    string Model,
+    int Year,
+    string Color,
+    decimal Price
+   );
+
+  public record NewCar(
+    string Make,
+    string Model,
+    int Year,
+    string Color,
+    decimal Price
+   );
+}
