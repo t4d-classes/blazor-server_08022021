7e1d61055f14f6152b7e46ffaf6e839fdcd6a04b
Eric Greene <eric@t4d.io>
2021-08-02 16:28:21 -0400

    add two new solutions

---
+++ ToolsApp/ToolsApp.Models/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ToolsApp.Models
+{
+  public class Class1
+  {
+  }
+}
---
+++ ToolsApp/ToolsApp.Services/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ToolsApp.Services
+{
+  public class Class1
+  {
+  }
+}
