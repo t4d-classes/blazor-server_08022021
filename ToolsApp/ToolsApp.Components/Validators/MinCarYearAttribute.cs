4df2cd6bfaf41ff8144787a1efe2ef8b3a245427
Eric Greene <eric@t4d.io>
2021-08-04 13:06:59 -0400

    add solution to exercise 10

---
+++ ToolsApp/ToolsApp.Components/Validators/MinCarYearAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ToolsApp.Components.Validators
+{
+  public class MinCarYearAttribute : ValidationAttribute
+  {
+    private int _minCarYear = 1886;
+
+    protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+    {
+      if (Convert.ToInt32(value) < _minCarYear)
+      {
+        return new ValidationResult(
+          $"{validationContext.DisplayName} is not a valid car year.",
+          new[] { validationContext.MemberName }
+        );
+      }
+
+      return ValidationResult.Success;
+    }
+  }
+}
