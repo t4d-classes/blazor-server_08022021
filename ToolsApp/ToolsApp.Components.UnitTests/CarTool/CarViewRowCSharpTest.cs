0b012cc23e5c5e383dd5dc0657d121bdc47b7444
Eric Greene <eric@t4d.io>
2021-08-05 14:10:35 -0400

    add exercise 16

---
+++ ToolsApp/ToolsApp.Components.UnitTests/CarTool/CarViewRowCSharpTest.cs
@@ -0,0 +1,36 @@
+﻿using Xunit;
+using Bunit;
+
+using ToolsApp.Components.CarTool;
+using ToolsApp.Models.Cars;
+
+namespace ToolsApp.Components.UnitTests.CarTool
+{
+  public class CarViewRowCSharpTest: TestContext
+  {
+    [Fact]
+    public void CarViewRowComponentRendersCorrectly()
+    {
+      // Arrange
+      var car = new Car(1, "Ford", "Fusion Hybrid", 2020, "blue", 45000.0M);
+
+      // Act
+      var cut = RenderComponent<CarViewRow>(parameters =>
+        parameters.Add(p => p.Car, car));
+
+      // Assert
+      cut.MarkupMatches(@"<tr>
+        <td>1</td>
+        <td>Ford</td>
+        <td>Fusion Hybrid</td>
+        <td>2020</td>
+        <td>blue</td>
+        <td>45000.0</td>
+        <td>
+          <button type=""button"">Edit</button>
+          <button type=""button"">Delete</button>
+        </td>
+      </tr>");
+    }
+  }
+}
---
+++ ToolsApp/ToolsApp.Components.UnitTests/Shared/ActionDataListCSharpTest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xunit;
+using Bunit;
+using Moq;
+
+using ToolsApp.Components.Shared;
+
+namespace ToolsApp.Components.UnitTests.Shared
+{
+  public class ActionDataListCSharpTest : TestContext
+  {
+    [Fact]
+    public void ActionDataListComponentActionButtonClicked()
+    {
+      // Arrange
+      List<string> colors = new()
+      {
+        "red",
+        "green",
+        "blue"
+      };
+      string emptyMessage = "No Colors";
+
+      var actionButtonLabel = "X";
+
+      var itemActionMock = Mock.Of<Action<string>>();
+
+      // Act
+      var cut = RenderComponent<ActionDataList>(parameters =>
+        parameters
+          .Add(p => p.Items, colors)
+          .Add(p => p.EmptyMessage, emptyMessage)
+          .Add(p => p.ActionButtonLabel, actionButtonLabel)
+          .Add(p => p.OnItemAction, itemActionMock)
+          .AddChildContent("<h3>Colors</h3>"));
+
+      var buttonElement = cut.Find("li:nth-child(2) button");
+
+      buttonElement.Click();
+
+      // Assert
+      Mock.Get(itemActionMock).Verify(mock => mock("green"), Times.Once());
+    }
+  }
+}
